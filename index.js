7c3e18f2fe485f2370703903021c94af8771cf88
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-05-20 22:14:52 +0600

    Assignment-11, batch-10, Programming Hero project setup; created a route to get all the historical artifacts.

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy5hpga.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const historicalArtifactsCollection = client.db("hATracker").collection("historicalArtifacts");
+
+    app.get('/historicalArtifacts', async(req, res) => {
+      const cursor = historicalArtifactsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("We are tracking historical artifacts.");
+})
+
+app.listen(port, () => {
+    console.log(`Historical artifacts are being tracked on: ${port}`);
+})
